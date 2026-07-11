d54f9ed6c24e6d916b346fdf921650a591275312
Liang Mi <github@liangmi.dev>
2026-07-11 08:53:38 +0800

    chore: setup basic code environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { lib } from '@liangmi/vp-config'
 
 export default lib({
-  lint: {
-    rules: {
-      'no-console': 'off'
-    }
-  },
   pack: {
-    entry: './src/index.ts'
+    entry: './src/index.ts',
+    platform: 'node'
   }
 })
